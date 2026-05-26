b05cfd5494221d699570d755b719342711c139e1
Raya <rayakuzmina73@gmail.com>
2026-05-26 22:22:17 +0300

    feat: create /token/refresh endpoint

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from auth_app.views import RegisterAPIView, LoginAPIView, LogoutAPIView
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('register/', RegisterAPIView.as_view()),
     path('login/', LoginAPIView.as_view()),
     path('logout/', LogoutAPIView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 ]
