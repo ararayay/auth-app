be1a2b161c8bb1e2c3ba11f8ddd3612750d20779
Raya <rayakuzmina73@gmail.com>
2026-05-27 23:45:07 +0300

    feat: create HasAccessPermission class

---
+++ auth_app/services/access.py
@@ -0,0 +1,19 @@
+from auth_app.models import RolePermission
+
+from rest_framework.permissions import BasePermission
+
+
+class HasAccessPermission(BasePermission):
+    resource = None
+    action = None
+
+    def has_permission(self, request, view):
+        user = request.user
+        if not user or not user.is_authenticated:
+            return False
+
+        return RolePermission.objects.filter(
+            role__userrole__user=user,
+            permission__resource=self.resource,
+            permission__action=self.action
+        ).exists()
